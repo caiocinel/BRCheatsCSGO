1761a8cecd9fda61c0aba95ec69bc89a704fe251
caiocinel <caioalencarcinel@gmail.com>
2020-12-07 17:21:15 -0300

    Remove garbage

--- Build.bat
+++
@@ -1,6 +0,0 @@
-SET VS=E:\Programas\VStudio
-SET PROJ=C:\Users\caioc\Desktop\BRCheats CS\Osiris
-CALL "%VS%\Common7\Tools\vcvars32.bat"
-cl.exe /EHsc "%PROJ%\program.cpp"
-IF NOT %ERRORLEVEL%==0 GOTO Done
-:Done
